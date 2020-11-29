f557002301980afff79253f71ecfa3a608f3de0b
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-11-28 21:55:43 -0800

    commit tree 0a3a31ed3f88196d26b8a895893722615000c380

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,32 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0,
+        };
+    }
+
+    clickFunc = () => {
+        this.setState( previousState=>({
+                timesClicked: previousState.timesClicked +1   
+            })
+        )
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickFunc}>{this.state.timesClicked}</button>
+            </div>
+        );
+    }
+}
+
+export default DigitalClicker;
+
+
+
